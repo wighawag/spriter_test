004540da8f6aadfeb2b398ef3552472b0dc8ad94
wighawag <wighawag@gmail.com>
2016-02-17 17:41:06 +0000

    use latest kha

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -5,7 +5,12 @@ import kha.System;
 
 class Main {
 	public static function main() {
-		System.init("spriter_test", 800, 600, initialized);
+		System.init({
+			title: "spriter_test",
+			width: 800,
+			height: 600,
+			samplesPerPixel: 1
+			}, initialized);
 	}
 	
 	private static function initialized(): Void {
